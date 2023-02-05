bc0ea98ea3a28d692e3bab0a4176cf7740f61d03
Trevor Vardeman <trevortx@gmail.com>
2023-02-04 16:03:18 -0800

    configuring react router for render

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,18 +11,16 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <Routes>
       <div className="App">
-        <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            <h1>Page Count: {count}</h1>
-          </Route>
-        </Switch>
+        <Route path="/testing">
+          <h1>Test Route</h1>
+        </Route>
+        <Route path="/">
+          <h1>Page Count: {count}</h1>
+        </Route>
       </div>
-    </BrowserRouter>
+    </Routes>
   );
 }
 
