ba955e8e349e25be915e51e6a9dae9727506e739
Trevor Vardeman <trevortx@gmail.com>
2023-02-27 22:32:43 -0800

    after changing avatar, now sending back user and avatar key to the front end

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :username, :avatar_key
 
   has_many :trips
   has_many :cities, through: :trips
+
+  def avatar_key
+    key = current_user.avatar.key
+    return key
+  end
 end
