0fdbea50112420842d17a5e91bed5d6eaece6a64
Trevor Vardeman <trevortx@gmail.com>
2023-03-11 03:12:30 -0800

    got the new TagSearch component working as expected, and can now create new tags and add existing tags to a trip at the same time

--- client/src/components/TagSearch.js
+++ client/src/components/TagSearch.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import { useUserUpdate } from '../context/UserContext'
+import { useTripContext } from '../context/CurrentTripContext'
 import CreatableSelect from 'react-select/creatable'
 import makeAnimated from 'react-select/animated'
 import Button from 'react-bootstrap/Button'
@@ -6,14 +8,12 @@ import Stack from 'react-bootstrap/Stack'
 import Spinner from 'react-bootstrap/Spinner'
 
 function TagSearch() {
-  const animatedComponents = makeAnimated()
-  const [allTags, setAllTags] = useState(null)
+  const userUpdate = useUserUpdate()
+  const { currentTrip, setCurrentTrip } = useTripContext()
+  const [unformattedTags, setUnformattedTags] = useState(null)
+  const [formattedTags, setFormattedTags] = useState(null)
   const [tripTags, setTripTags] = useState(null)
-  const handleSubmit = e => {
-    e.preventDefault()
-    console.log(e)
-    setTripTags(null)
-  }
+  const animatedComponents = makeAnimated()
 
   useEffect(() => {
     fetch("/tags", {
@@ -22,18 +22,81 @@ function TagSearch() {
     })
     .then(r => r.json())
     .then(tags => {
+      setUnformattedTags(tags)
       const tagFormat = tags.map(tag => {
         let newTag = {
           label: tag.name, value: tag.name
         }
         return newTag
       })
-      setAllTags(tagFormat)
+      setFormattedTags(tagFormat)
     })
     .catch(e => alert(e))
   },[])
 
-  if (!allTags) {
+  const handleSubmit = e => {
+    e.preventDefault()
+    const new_tags = tripTags.filter(tag => tag.__isNew__ === true).map(tag => {
+      let tagObj = {
+        name: tag.label.toLowerCase(),
+        trip_id: currentTrip.id
+      }
+      return tagObj
+    })
+
+    const existingTags = tripTags.filter(tag => Object.keys(tag).length < 3)
+    const existing_trip_tags = existingTags.map(existingTag => {
+      let tags = unformattedTags.filter(tag => tag.name === existingTag.label)
+      let id =  tags.map(tag => tag.id)[0]
+      let tagObj = {
+        tag_id: id,
+        trip_id: currentTrip.id
+      }
+      return tagObj
+    })
+
+    if (new_tags.length > 0) {
+      fetch("/tags", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", },
+        body: JSON.stringify({ new_tags })
+      })
+      .then(r => {
+        if (r.ok) {
+          r.json().then(user => {
+            setTripTags(null)
+            userUpdate(user)
+            const updatedTrip = user.trips.filter(trip => trip.id === currentTrip.id)[0]
+            setCurrentTrip(updatedTrip)
+          })
+        } else {
+          r.json().then(r => alert(r.error))
+        }
+      })
+      .catch(e => alert(e))
+    } else if (existing_trip_tags.length > 0) {
+      fetch("/trip_tags", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", },
+        body: JSON.stringify({ existing_trip_tags })
+      })
+      .then(r => {
+        if (r.ok) {
+          r.json().then(user => {
+            setTripTags(null)
+            userUpdate(user)
+            const updatedTrip = user.trips.filter(trip => trip.id === currentTrip.id)[0]
+            setCurrentTrip(updatedTrip)
+          })
+        } else {
+          r.json().then(r => alert(r.error))
+        }
+      })
+      .catch(e => alert(e))
+    } else return
+  }
+
+  if (!formattedTags) {
     return (
       <Spinner className="definite-center" animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
@@ -47,7 +110,7 @@ function TagSearch() {
         closeMenuOnSelect={false}
         components={animatedComponents}
         isMulti
-        options={allTags}
+        options={formattedTags}
         onChange={e => setTripTags(e)}
         value={tripTags}
       />
